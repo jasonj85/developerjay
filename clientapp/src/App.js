b86a2ea4e34d0fe515e97d1167133ac78d9402c5
JJ <jasonj85@gmail.com>
2019-03-10 11:52:53 +0000

    Added react skeleton

---
+++ clientapp/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+// import components
+import Navbar from "./components/layout/Navbar.js";
+import Footer from "./components/layout/Footer.js";
+import Landing from "./components/layout/Landing.js";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
