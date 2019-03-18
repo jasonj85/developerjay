10e9cce2eea3a08e7193afce676e263d65dfddf0
JJ <jasonj85@gmail.com>
2019-03-18 22:28:41 +0000

    Updated redux extension tool

--- clientapp/src/store.js
+++ clientapp/src/store.js
@@ -3,24 +3,15 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
+
 const middleware = [thunk];
 
-// only run redux dev tools when using dev environment
-const devTools =
-  process.env.NODE_ENV === "development"
-    // @ts-ignore
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      // @ts-ignore
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : null;
-
-// set redux store
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    devTools
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
