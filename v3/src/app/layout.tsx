d0c53b2fbc8880385a947a3d9d113aa078e42751
Jason James <jason.james@cba-japan.com>
2025-01-10 19:07:06 -0500

    initial base project

---
+++ v3/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Jason James",
+  description: "Developer Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.className}`}>{children}</body>
+    </html>
+  );
+}
