8f7a16afb835da0ef0f35684995feb1a361cbd49
Jason James <jason.james@cba-japan.com>
2025-01-20 16:44:25 -0300

    add icons and basic info

---
+++ v3/src/assets/index.tsx
@@ -0,0 +1,17 @@
+// Hero
+import InstagramLineIcon from 'remixicon-react/InstagramLineIcon'
+import FacebookCircleLineIcon from 'remixicon-react/FacebookCircleLineIcon'
+import DribbbleLineIcon from 'remixicon-react/DribbbleLineIcon'
+import YoutubeLineIcon from 'remixicon-react/YoutubeLineIcon'
+import GithubLineIcon from 'remixicon-react/GithubLineIcon'
+
+/* eslint-disable react/jsx-key */
+export const heroIcons = [
+  <InstagramLineIcon />,
+  <FacebookCircleLineIcon />,
+  <DribbbleLineIcon />,
+  <YoutubeLineIcon />,
+  <GithubLineIcon />,
+]
+
+// About Me
--- v3/src/components/hero.tsx
+++ v3/src/components/hero.tsx
@@ -1,24 +1,31 @@
 // tell nextjs to render on the client side
 "use client";
 import Image from "next/image";
+import { heroIcons } from "@/assets";
 
 const Hero = () => {
   return (
     <div className="h-screen grid place-items-center">
       <div>
-        <div>
-          <div>
-            <Image src={"/person-wave.png"} alt="Person Image" width={400} height={400} priority={true} />
-            <span>Hi</span>
+        <div className="flex flex-col items-center justify-center gap-y-3 font-light capitalize">
+          <div className="flex items-center justify-center">
+            <Image src={"/person-wave.png"} alt="Person Image" width={400} height={400} priority={true} className="h-auto w-[200px]" />
+            <span className="absolute text-xl font-semibold text-white mt-3">Hi</span>
           </div>
-          <h1>My Name is Jason James &</h1>
-          <p>I like web development 👨🏽‍💻</p>
+          <h1 className="text-center text-3xl font-bold tracking-wider text-gray-500">My Name is Jason James &</h1>
+          <p className="text-lg tracking-wider text-gray-700">I like web development 👨🏽‍💻</p>
+        </div>
+        <div className="mt-8 flex justify-center gap-x-10 text-3xl text yellow-600">
+          {heroIcons.map((icon, i) => (
+            <a href="#" key={i} className="rounded-lg hover:bg-red-400 hover:-text-white transition-colors">
+              {icon}
+            </a>
+          ))}
         </div>
         <div>
-          <a href="#">Icon</a>
-        </div>
-        <div>
-          <a href="#">Talk to me</a>
+          <a href="#" className="mt-7 block w-max rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider mx-auto text-white hover:bg-red-500 transition-colors">
+            Talk to me
+          </a>
         </div>
       </div>
     </div>
