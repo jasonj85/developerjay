f467764053deecbcbec6f18956e93703c84dbb5a
Jason James <jason.james@cba-japan.com>
2025-01-24 17:44:39 -0300

    add some animations to experience timeline

--- v3/src/components/Experience.tsx
+++ v3/src/components/Experience.tsx
@@ -3,9 +3,23 @@
 import Heading from "./sub/Heading";
 import Image from "next/image";
 import { arrowLeftIcon, experienceData } from "@/assets";
+import { motion, useScroll, useSpring } from "framer-motion";
+import { useRef } from "react";
 
 const Experience = () => {
   const dateYear = new Date().getFullYear();
+
+  const containerRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start 95%", "end end"],
+  });
+
+  const scrollY = useSpring(scrollYProgress, {
+    stiffness: 200,
+    damping: 20,
+  });
 
   return (
     <div className="py-20 px-96 relative">
@@ -13,11 +27,14 @@ const Experience = () => {
       <Image
         src={"/education.png"}
         alt={"Experience image"}
-        width={400}
-        height={400}
+        width={300}
+        height={300}
         className="absolute -top-4 right-96 opacity-70 lg:hidden"
       />
-      <div className="w-full h-full flex flex-col items-center justofy-center gap-y-10 lg:gap-y-20 py-10">
+      <div
+        ref={containerRef}
+        className="w-full h-full flex flex-col items-center justofy-center gap-y-10 lg:gap-y-20 py-10"
+      >
         {experienceData.map((data, i) => (
           <div
             key={i}
@@ -27,7 +44,13 @@ const Experience = () => {
                 : "left-[300px] xl:-left-[240px] lg:left-0"
             }`}
           >
-            <div className="relative flex flex-col gap-y-3 rounded-md border border-red-200 bg-white p-4 tracking-wide sm:text-sm">
+            <motion.div
+              initial={{ opacity: 0, x: i % 2 === 0 ? -80 : 80 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.7, type: "spring", stiffness: 50 }}
+              className="relative flex flex-col gap-y-3 rounded-md border border-red-200 bg-white p-4 tracking-wide sm:text-sm"
+            >
               <h1 className="text-xl sm:text-lg font-light text-gray-700">{data.title}</h1>
               <p className="text-gray-500">
                 <span className="block font-light">Education:</span>
@@ -49,17 +72,25 @@ const Experience = () => {
               >
                 {arrowLeftIcon}
               </span>
-            </div>
+            </motion.div>
             <div
               className={`absolute top-20 w-14 border border-gray-400 rounded-full aspect-square grid place-items-center text-red-400 font-light -translate-y-1/2 z-10 bg-gray-100
-                ${i % 2 === 0 ? "left-full -translate-x-1/2 lg:left-1/2" : "right-full translate-x-1/2 lg:right-1/2 "}`}
+                ${
+                  i % 2 === 0
+                    ? "left-full -translate-x-1/2 lg:left-1/2"
+                    : "right-full translate-x-1/2 lg:right-1/2 "
+                }`}
             >
               {dateYear - experienceData.length + i + 1}
             </div>
           </div>
         ))}
 
-        <div className="absolute w-1 h-full rounded-full bg-gray-300"></div>
+        <motion.div
+          initial={{ scaleY: 0 }}
+          style={{ scaleY: scrollY }}
+          className="absolute w-1 h-full rounded-full bg-gray-300 origin-top"
+        ></motion.div>
       </div>
     </div>
   );
