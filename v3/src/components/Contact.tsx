5285157951edc899cc8a166159921b50e8c086bf
Jason James <jason.james@cba-japan.com>
2025-01-29 17:15:07 -0300

    adding contact section

---
+++ v3/src/components/Contact.tsx
@@ -0,0 +1,65 @@
+// tell nextjs to render on the client side
+"use client";
+import Image from "next/image";
+import Heading from "./sub/Heading";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  return (
+    <div className="h-screen py-20 px-96">
+      <Heading text={"Contact me"} />
+      <div className="w-full h-full my-auto flex lg:flex-col items-center justify-between lg:justify-center gap-x-20 lg:gap-x-0 gap-y-20">
+        <motion.div
+          initial={{ opacity: 0, y: 150 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4 }}
+          viewport={{ once: true }}
+        >
+          <Image
+            src={"/contact-me.png"}
+            alt="Contact me image"
+            width={400}
+            height={400}
+            className="w-[400px] rounded-md opacity-80 transform -scale-x-100"
+          />
+        </motion.div>
+        <motion.form
+          initial={{ opacity: 0, x: 150 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.4 }}
+          viewport={{ once: true }}
+          className="w-[600px] lg:w-[400px] sm:w-full flex flex-col gap-3"
+        >
+          <div className="w-full flex lg:flex-col gap-x-3">
+            <input
+              type="text"
+              className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-200 outline-none mb-2"
+              placeholder="Your Name"
+            />
+            <input
+              type="email"
+              className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-200 outline-none"
+              placeholder="Your Email"
+            />
+          </div>
+          <input
+            type="text"
+            className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-200 outline-none"
+            placeholder="Subject"
+          />
+          <textarea
+            className="max-h-[250px] min-h-[150px] border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-200 outline-none"
+            placeholder="Your message"
+          ></textarea>
+          <input
+            type="submit"
+            className="w-full border border-yellow-700 bg-yellow-600 rounded-md px-4 py-2 text-sm tracking-wider text-white outline-none hover:bg-yellow-500 transition-colors cursor-pointer"
+            value="Send Message"
+          />
+        </motion.form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
